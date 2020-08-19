e9cb19ac02be8675e5f781a962e912b464a3b457
paagalpegasi <alpha.pegasi@outlook.com>
2020-08-20 02:32:57 +0530

    init focus

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/focus.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <title>Focus LMS</title>
+</head>
+<body>
+    <header class="site-header is-sticky">
+        <nav class="navbar navbar-full navbar-expand-md is-transparent sticky-top" id="mainnav">
+            <div class="navbar-brand">
+                <img class="logo" src="lib/logo-big.png" alt="Cryptico Zinnia">
+            </div> 
+            
+            <button class="navbar-toggler" type="button">
+            <span class="navbar-toggler-icon">
+                <span class="ti ti-align-justify"></span>
+            </span>
+            </button> 
+            
+            <div class="collapse navbar-collapse justify-content-between">
+                <ul id="menu-onepage-menu" class="navbar-nav menu-top">
+                    <li id="menu-item-1225" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1225">
+                        <a href="#all_programs" class="nav-link">All Programs</a></li>
+                    <li id="menu-item-1246" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1246">
+                        <a href="#upskill_program" class="nav-link">Upskill Program</a></li>
+                    <li id="menu-item-1227" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1227">
+                        <a href="#executive_masters" class="nav-link">Executive Masters</a></li>
+                    <li id="menu-item-1230" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1230">
+                        <a href="#higher_degree" class="nav-link">Higher Degree</a></li>
+                    <li id="menu-item-1230" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1230">
+                        <a href="#corporate_connect" class="nav-link">Corporate Connect</a></li>
+                    <li id="menu-item-1230" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1230">
+                        <a href="#university_partners" class="nav-link">University Partners</a></li>
+                </ul>
+                
+                <ul class="navbar-nav navbar-btns">                            
+                    <li class="nav-item"><a class="nav-link btn btn-sm btn-outline menu-link" href="#">Sign Up</a></li>
+                    <li class="nav-item"><a class="nav-link btn btn-sm btn-outline menu-link" href="#">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section> section </section>
+        <section> section </section>
+        <section> section </section>
+        <section> section </section>
+    </main>
+</body>
+</html>
